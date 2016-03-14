df08bd5e7e772062494591d978aab3260bd79b0f
AMAN3003 <iitp.aman@gmail.com>
2016-03-15 01:48:00 +0530

    created __init__ in main package

---
+++ aman_py/__init__.py
@@ -0,0 +1,10 @@
+
+"""easy to use synthetic data ."""
+
+from .Synthetic_Data import address
+from .Synthetic_Data import basic
+from .Synthetic_Data import currency
+from .Synthetic_Data import date
+from .Synthetic_Data import internet
+from .Synthetic_Data import name
+from .Synthetic_Data import personal
